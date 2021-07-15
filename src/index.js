d06aac02dc0c5b6584b0bce79be6d102f491b831
Tatsiana Dashuk <t_korbyt@mail.ru>
2021-07-15 15:31:15 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -35,11 +35,55 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+  let array = [];
+  while (expr.length > 0) {
+    let arr = expr.slice(0, 10);
+    let a = arr.split("").findIndex((item) => item === "1");
+    let b = arr.split("");
+    b.splice(0, a);
+    expr = expr.replace(arr, "");
+    array.push(b.join(""));
+  }
+
+  let symbol = [];
+  let slovo = [];
+  let SlovoMorse = [];
+
+  for (let i = 0; i < array.length; i++) {
+    symbol = array[i];
+    slovo = [];
+
+    let dot;
+    let dush;
+
+    while (symbol.length > 0) {
+      for (let j = 0; j <= symbol.length; j++) {
+        let arrSymbol = symbol.slice(0, 2);
+        symbol = symbol.replace(arrSymbol, "");
+        slovo.push(arrSymbol);
+
+        dot = slovo.map((item) => (item === "10" ? "." : item));
+        dush = dot.map((item) => (item === "11" ? "-" : item));
+      }
+    }
+
+    SlovoMorse.push(dush.join(""));
+  }
+
+  for (let i = 0; i < SlovoMorse.length; i++) {
+    for (var key in MORSE_TABLE) {
+      if (SlovoMorse[i] === key) {
+        SlovoMorse[i] = MORSE_TABLE[key];
+      }
+    }
+  }
+  return SlovoMorse.join("");
 }
+
 
 module.exports = {
     decode
